7ba7643f052848c22e8fa4a2f06418b2f73321c0
yashika0128 <chaubeyyashika3@gmail.com>
2026-04-09 10:18:41 +0530

    Lecture 08- > Pattern Printing part 02

---
+++ src/Lecture07_Patterns/Part02/p1.java
@@ -0,0 +1,24 @@
+package Lecture07_Patterns.Part02;
+
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        // Hollow rectangle
+
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+
+        for (int i = 1; i <= row ; i++) {
+            for (int j = 1; j <= col; j++) {
+                if(i==1 || j==1|| i==row|| j==col) {
+                    System.out.print("*");
+                }
+                else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
